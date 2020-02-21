b4db98c3c7fd5bb4de222e9d51191845ea91da53
Nina Potrebich <potrebich@gmail.com>
2020-02-21 10:15:38 -0800

    Update KegList component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,15 +11,99 @@ import {v4} from 'uuid';
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    const newMasterKegList = {};
+
+    let newKegId = v4();
+    newMasterKegList[newKegId] = {
+      name: "Gold", 
+      brand: "Saint Archer Brewing Company", 
+      price: 7.00, 
+      alcoholContent: 4.2
+    }
+
+    newKegId = v4();
+    newMasterKegList[newKegId] = {
+      name: "Insignia",
+      brand: "Mystic Brewery",
+      price: 8.50,
+      alcoholContent: 5.0
+    }
+
+    newKegId = v4();
+    newMasterKegList[newKegId] = {
+      name: "Easy Jack",
+      brand: "Firestone Walker Brewing Company",
+      price: 7.50,
+      alcoholContent: 4.0,
+      ibu: 47
+    }
+
+    newKegId = v4();
+    newMasterKegList[newKegId] = {
+      name: "Pale Ale",
+      brand: "Sierra Nevada Brewing Co.",
+      price: 7.00,
+      alcoholContent: 5.6,
+      ibu: 38
+    }
+
+    newKegId = v4();
+    newMasterKegList[newKegId] = {
+      name: "Golden Monkey",
+      brand: "Victory Brewing Company",
+      price: 8.00,
+      alcoholContent: 9.5,
+      ibu: 25
+    }
+
+    newKegId = v4();
+    newMasterKegList[newKegId] = {
+      name: "Margarita Gose",
+      brand: "Cigar City Brewing",
+      price: 7.50,
+      alcoholContent: 4.2,
+      ibu: 5
+    }
+
+    newKegId = v4();
+    newMasterKegList[newKegId] = {
+      name: "Fat Tire",
+      brand: "New Belgium Brewing Company",
+      price: 7.00,
+      alcoholContent: 5.2,
+      ibu: 22
+    }
+
+    newKegId = v4();
+    newMasterKegList[newKegId] = {
+      name: "Guinness Draught",
+      brand: "Guinness",
+      price: 8.00,
+      alcoholContent: 4.2,
+      ibu: 45
+    }
+
+    newKegId = v4();
+    newMasterKegList[newKegId] = {
+      name: "Ginger Root Kombucha",
+      brand: "Urban Farm Fermentory",
+      price: 9.00,
+      alcoholContent: 1.5
+    }
+
     this.state = {
-      masterKegList: {},
+      masterKegList: newMasterKegList,
       selectedKeg: null
     }
+    this.handleAddingNewKegToList = this.handleAddingNewKegToList.bind(this);
+    this.handleChangingSelectedKeg = this.handleChangingSelectedKeg.bind(this);
   }
 
-  handleAddingNewKegToList = (newKeg) => {
+  handleAddingNewKegToList(newKeg) {
     const newKegId = v4();
     const newMasterKegList = Object.assign({}, this.state.masterKegList, {[newKegId]: newKeg});
+    // const newMasterKegList = {...this.state.masterKegList, [newKegId]: newKeg};
     this.setState({masterKegList: newMasterKegList});
   }
 
--- src/components/KegList.jsx
+++ src/components/KegList.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import backgroundImage from '../assets/images/kegList-background.jpeg'
 import Keg from './Keg';
 import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
@@ -29,29 +28,28 @@ const KegList = (props) => {
                 }
             `}</style>
       <div className="text-box">
-        {props.employee ? <Link to="/new">Add a new keg</Link> : null}
-        <br/>
-        <br/>
         <h1>List of available kegs:</h1>
         <div className="kegs-list">
-          <Keg name="Gold" brand="Saint Archer Brewing Company" price={7.00} alcoholContent={4.2} employee={props.employee}/>
-          <Keg name="Insignia" brand="Mystic Brewery" price={8.50} alcoholContent={5.0} employee={props.employee}/>
-          <Keg name="Easy Jack" brand="Firestone Walker Brewing Company" price={7.50} alcoholContent={4.0} ibu={47} employee={props.employee}/>
-          <Keg name="Pale Ale" brand="Sierra Nevada Brewing Co." price={7.00} alcoholContent={5.6} ibu={38} pints={124}employee={props.employee}/>
-          <Keg name="Golden Monkey" brand="Victory Brewing Company" price={8.00} alcoholContent={9.5} ibu={25} employee={props.employee}/>
-          <Keg name="Margarita Gose" brand="Cigar City Brewing" price={7.50} alcoholContent={4.2} ibu={5} employee={props.employee}/> 
-          <Keg name="Fat Tire" brand="New Belgium Brewing Company" price={7.00} alcoholContent={5.2} ibu={22} employee={props.employee}/>
-          <Keg name="Guinness Draught" brand="Guinness" price={8.00} alcoholContent={4.2} ibu={45} employee={props.employee}/>
-          <Keg name="Ginger Root Kombucha" brand="Urban Farm Fermentory" price={9.00} alcoholContent={1.5} employee={props.employee}/>
+          {Object.keys(props.kegList).map(function(kegId)
+            {
+              var keg = props.kegList[kegId];
+              return <Keg name={keg.name} 
+                brand={keg.brand} 
+                price={keg.price} 
+                alcoholContent={keg.alcoholContent} 
+                ibu={keg.ibu}/>
+            }
+          )}
         </div>
       </div>
 
     </div>
   );
+  
 };
 
 KegList.propTypes = {
-  employee: PropTypes.bool,
+  kegList: PropTypes.object.isRequired
 };
 
 
