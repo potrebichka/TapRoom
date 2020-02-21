c99a2f95e227efb2a980f2566767206035b9bd19
Nina Potrebich <potrebich@gmail.com>
2020-02-21 09:03:28 -0800

    Update Routes links in App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,34 +4,30 @@ import Header from './Header';
 import Main from './Main';
 import NotFound from './NotFound';
 import KegList from './KegList';
-import NewKeg from './NewKeg';
-import EditKeg from './EditKeg';
-import Account from './Account';
 import wood_background from '../assets/images/wood_background.jpg';
 
 
 const App = () => {
-  return (
-    <div className="page_background">
-      <style jsx>{`
-                .page_background {
-                    background: url(${wood_background}) no-repeat center center fixed;
-                    min-height: 100vh;
-                    background-size: cover;
-                    overflow: hidden;
-                }
-            `}</style>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={Main} />
-        <Route path='/list' component={KegList} />
-        <Route path='/new' component={NewKeg} />
-        <Route path='/edit' component={EditKeg} />
-        <Route path='/account' component={Account} />
-        <Route component={NotFound} />
-      </Switch>
-    </div>
-  );
+    return (
+      <div className="page_background">
+        <style jsx>{`
+                  .page_background {
+                      background: url(${wood_background}) no-repeat center center fixed;
+                      min-height: 100vh;
+                      background-size: cover;
+                      overflow: hidden;
+                  }
+              `}</style>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Main} />
+          <Route path='/list' render={() => <KegList employee={false}/>} />
+          <Route path='/account' render={() => <KegList employee={true}/>} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    );
+  }
 };
 
 export default App;
