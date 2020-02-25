5c74c44dac679d2d3029996d738d253fb105cfb3
Nina Potrebich <potrebich@gmail.com>
2020-02-24 23:12:28 -0800

    Updates

--- index.jsx
+++ index.jsx
@@ -7,9 +7,9 @@ import 'bootstrap/dist/css/bootstrap.css';
 const render = Component => {
   ReactDOM.render(
 
-      <HashRouter>
-        <Component />
-      </HashRouter>,
+    <HashRouter>
+      <Component />
+    </HashRouter>,
     document.getElementById('react-app-root')
   );
 };
